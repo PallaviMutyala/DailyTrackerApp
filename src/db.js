10d583c0db852fc9d16e9a4e72f9bdfaeb7974c1
Pallavi Mutyala <pmutyala@Pallavis-MacBook-Pro.local>
2026-05-19 14:02:50 -0700

    fix supabase url

--- src/db.js
+++ src/db.js
@@ -8,7 +8,7 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
 // ⚠️ Replace these with your project's values from Supabase → Settings → API
-const SUPABASE_URL      = 'https://habxxavgajszxxtqdwie.supabase.co/rest/v1/';
+const SUPABASE_URL      = 'https://habxxavgajszxxtqdwie.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhhYnh4YXZnYWpzenh4dHFkd2llIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyMTM1NDAsImV4cCI6MjA5NDc4OTU0MH0.Pw9KmS7WQ7hbyH-bXO4rYupdL9eIHh_rGvPBUVHxZcE';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
